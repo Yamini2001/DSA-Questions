c75c87e183f249f74ca967f299496baa412f8e50
BeSingularTeam <besingulardemo@gmail.com>
2025-06-02 18:24:39 +0530

    commit

---
+++ April/RunningSum.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int n = nums.length;
+        int[] prefix = new int[n];
+        prefix[0] = nums[0];
+        for(int i=1;i<n;i++){
+            prefix[i] = prefix[i-1] + nums[i];
+
+        }
+        return prefix;
+    }
+}
