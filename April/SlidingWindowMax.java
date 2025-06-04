2a2555651b2eebf581dfd394fa5f89d7d16c9d75
BeSingularTeam <besingulardemo@gmail.com>
2025-06-04 19:15:54 +0530

    commit

---
+++ April/SlidingWindowMax.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        List<Integer> res = new ArrayList<>();
+        Deque<Integer> deque = new LinkedList<>();
+
+        for (int idx = 0; idx < nums.length; idx++) {
+            int num = nums[idx];
+
+            while (!deque.isEmpty() && deque.getLast() < num) {
+                deque.pollLast();
+            }
+            deque.addLast(num);
+
+            if (idx >= k && nums[idx - k] == deque.getFirst()) {
+                deque.pollFirst();
+            }
+
+            if (idx >= k - 1) {
+                res.add(deque.getFirst());
+            }
+        }
+
+        return res.stream().mapToInt(i -> i).toArray();        
+    }
+}
