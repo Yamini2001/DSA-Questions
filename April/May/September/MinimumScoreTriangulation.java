50c68c22d7a5460506545f5dab251a61247b515e
BeSingularTeam <besingulardemo@gmail.com>
2025-09-29 14:34:20 +0530

    commit

--- April/May/September/MinimumScoreTriangulation.java
+++ April/May/September/MinimumScoreTriangulation.java
@@ -1,15 +1,15 @@
 class Solution {
-    int[][] dp = new int[50][50];
-    
-    public int minScoreTriangulation(int[] values, int i, int j, int res) {
-        if (j == 0) j = values.length - 1;
-        if (dp[i][j] != 0) return dp[i][j];
-        for (int k = i + 1; k < j; k++) {
-            res = Math.min(res == 0 ? Integer.MAX_VALUE : res,
-                minScoreTriangulation(values, i, k, 0) +
-                values[i] * values[k] * values[j] +
-                minScoreTriangulation(values, k, j, 0));
+    public int minScoreTriangulation(int[] values) {
+        int n = values.length;
+        int[][] dp = new int[n][n];
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                for (int k = i + 1; k < j; ++k) {
+                    dp[i][j] = Math.min(dp[i][j] == 0 ? Integer.MAX_VALUE : dp[i][j],
+                        dp[i][k] + values[i] * values[k] * values[j] + dp[k][j]);
+                }
+            }
         }
-        return dp[i][j] = res;
+        return dp[0][n - 1];
     }
 }
